872cbf2a28cba9e15b1fc4ceec591c6e9d7b8980
Diego Román-Cortés <diego.roman.c@ing.uchile.cl>
2023-05-24 14:22:10 -0400

    Update fano2d.py
    Add LICENSE

--- fano2d.py
+++ fano2d.py
@@ -1,3 +1,22 @@
+#    fano2d.py simulates the scattering problem of a plane wave encountering a central linear defect
+#    Copyright (C) 2023 Diego Román-Cortés
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#       
+#    Contact: diego.roman.c@ug.uchile.cl
+
+
 from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 import matplotlib as mpl
